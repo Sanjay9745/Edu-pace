995bae02a9c5710fae478c9bf312941bdbc9399b
vyshnaviprasee <aljoantonyplusitpark@gmail.com>
2023-12-28 12:38:01 +0530

    updated

---
+++ vocational_training.php
@@ -0,0 +1,495 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Vocation Training Program</title>
+  <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/5.0.0-alpha1/css/bootstrap.min.css" integrity="sha384-r4NyP46KrjDleawBgD5tp8Y7UzmLA05oM1iAEQ17CSuDqnUK2+k9luXQOfXJCJ4I" crossorigin="anonymous">
+  <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js" integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous"></script>
+  <script src="https://stackpath.bootstrapcdn.com/bootstrap/5.0.0-alpha1/js/bootstrap.min.js" integrity="sha384-oesi62hOLfzrys4LxRF63OJCXdXDipiYWBnvTl9Y9/TRlw5xlKIEHpNyvvDShgf/" crossorigin="anonymous"></script>
+  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet">
+  <link rel="stylesheet" href="./css/testimonial.css">
+
+  <link rel="stylesheet" href="./css/vocation.css">
+  <link rel="stylesheet" href="./css/info.css">
+</head>
+<body>
+
+<?php include("header/header.php") ?>
+<div id="heroCarousel" class="carousel slide" data-bs-ride="carousel">
+    <div class="carousel-inner">
+      <div class="carousel-item active">
+        <img src="./assets/slider/Doctors to Spain_ Edupace Language Academy_Thrissur.jpg" class="d-block w-100" alt="Slide 1">
+        <div class="carousel-caption d-none d-md-block">
+          
+        </div>
+      </div>
+      <div class="carousel-item">
+        <img src="./assets/slider/slider-2.jpg" class="d-block w-100" alt="Slide 2">
+        <div class="carousel-caption d-none d-md-block">
+          
+        </div>
+      </div>
+      <div class="carousel-item">
+        <img src="./assets/slider/slider.jpg" class="d-block w-100" alt="Slide 3">
+        <div class="carousel-caption d-none d-md-block">
+          
+        </div>
+      </div>
+    </div>
+  
+    <!-- Navigation buttons at the bottom -->
+    <div class="carousel-controls">
+      <button class="carousel-control-prev" type="button" data-bs-target="#heroCarousel" data-bs-slide="prev">
+        <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+        <span class="visually-hidden"></span>
+      </button>
+      <button class="carousel-control-next" type="button" data-bs-target="#heroCarousel" data-bs-slide="next">
+        <span class="carousel-control-next-icon" aria-hidden="true"></span>
+        <span class="visually-hidden"></span>
+      </button>
+    </div>
+  
+</div>
+
+<div class="vocation-container ">
+    <div class="vocation-head container">
+         <h1>Vocation Training Program In Spain</h1>
+    </div>
+    
+    <div class="vocation-content-top container">
+       <div class="vocation-content-left">
+        <h2>About Program </h2>
+        <p >Donec ullamcorper nulla non metus auctor fringilla. Vestibulum id ligula porta felis euismod
+          semper. Praesent commodo cursus magna, vel scelerisque nisl consectetur. Fusce dapibus, tellus ac cursus
+          commodo.</p>
+
+       </div> 
+
+       <div class="vocation-content-right">
+        <div class="flip-card" id="card-1-flip">
+            <div class="flip-card-inner">
+              <div class="flip-card-front">
+                <iframe width="100%" height="100%" src="https://www.youtube.com/embed/VCPGMjCW0is?autoplay=1&controls=0" frameborder="0" allowfullscreen></iframe>
+                <button type="button" class="btn btn flip-btn-1" id="flip-btn-1">Click Here</button>
+                
+              </div>
+              <div class="flip-card-back">
+                <ul>
+                    
+                    <li>Maecenas at lorem ac tortor posuere finibus.</li>
+                    <li>Nam rutrum purus eget massa viverra egestas.</li>
+                    <li>Sed vitae ex sed dolor cursus fermentum.</li>
+                  </ul>
+                  <button type="button" class="btn btn-primary flip-btn-1" id="flip-btn-2">Button</button>
+              </div>
+            </div>
+        </div>
+       </div>
+        
+    </div>
+    <div class="vocation-content-bottom container">
+       <div class="vocation-content-left">
+        <h2>First featurette heading. It’ll blow your mind.</h2>
+        <p >Donec ullamcorper nulla non metus auctor fringilla. Vestibulum id ligula porta felis euismod
+          semper. Praesent commodo cursus magna, vel scelerisque nisl consectetur. Fusce dapibus, tellus ac cursus
+          commodo.</p>
+
+       </div> 
+
+       <div class="vocation-content-right">
+        <div class="flip-card " id="card-2-flip">
+            <div class="flip-card-inner">
+              <div class="flip-card-front">
+                <iframe width="100%" height="100%" src="https://www.youtube.com/embed/VCPGMjCW0is?autoplay=1&controls=0" frameborder="0" allowfullscreen></iframe>                
+                <button type="button" class="btn btn-primary" id="flip-btn-3">Button</button>
+
+                
+              </div>
+              <div class="flip-card-back">
+                <ul>
+                    
+                    <li>Maecenas at lorem ac tortor posuere finibus.</li>
+                    <li>Nam rutrum purus eget massa viverra egestas.</li>
+                    <li>Sed vitae ex sed dolor cursus fermentum.</li>
+                  </ul>
+                  <button type="button" class="btn btn-primary" id="flip-btn-4">Button</button>
+              </div>
+            </div>
+        </div>
+       </div>
+        
+    </div>
+    <div class="vocation-para container">
+        <p>Donec ullamcorper nulla non metus auctor fringilla. Vestibulum id ligula porta felis euismod
+            semper. Praesent commodo cursus magna, vel scelerisque nisl consectetur. Fusce dapibus, tellus ac cursus
+            commodo.Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore 
+            magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo 
+            consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. 
+            Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+
+    </div>
+
+    <!-- infographics -->
+
+    <div class="vocation-infographics">
+    <div class="infogrphics-head container">
+        <h1>Ordered List Arrow Cards</h1>
+
+    </div>
+    <div class="infogrphics-content container">
+
+        <div class="info-left">
+            <h3>Lorem Ipsum</h3>
+            <p >Lorem ipsum dolor sit amet consectetur adipisicing elit. Accusantium, voluptatem.</p>
+            <ul>
+                <li>Maecenas at lorem ac tortor posuere finibus.</li>
+                <li>Nam rutrum purus eget massa viverra egestas.</li>
+                <li>Sed vitae ex sed dolor cursus fermentum.</li>
+                <li>Nullam eget nibh a mauris posuere tincidunt.</li>
+            </ul>
+        </div>
+        <div class="info-right">
+        <ol class="olcards">
+            <li style="--cardColor:#fc374e">
+                <div class="content">
+                    
+                    <div class="title">Lorem Ipsum</div>
+                    <div class="text">Lorem ipsum dolor sit amet consectetur adipisicing elit. Accusantium, voluptatem.</div>
+                </div>
+            </li>
+            <li style="--cardColor:#36aeb3">
+                <div class="content">
+                    
+                    <div class="title">Lorem Ipsum</div>
+                    <div class="text">Lorem ipsum dolor sit amet consectetur adipisicing elit. Accusantium, voluptatem.</div>
+                </div>
+            </li>
+            <li style="--cardColor:#162d59">
+                <div class="content">
+                    
+                    <div class="title">Lorem Ipsum</div>
+                    <div class="text">Lorem ipsum dolor sit amet consectetur adipisicing elit. Accusantium, voluptatem.</div>
+                </div>
+            </li>
+            <li style="--cardColor:#f15f0e">
+                <div class="content">
+                    
+                    <div class="title">Lorem Ipsum</div>
+                    <div class="text">Lorem ipsum dolor sit amet consectetur adipisicing elit. Accusantium, voluptatem.</div>
+                </div>
+            </li>
+        </ol>
+        </div>
+    
+    </div>
+    </div>
+
+   
+    <!-- faq -->
+
+      <div class="about-faq">
+        <div class="about-faq-section1">
+          <h1>Frequently asked questions</h1>
+          <p>Everything you need to know about the product and billing</p>
+        </div>
+        <div class="about-faq-section2">
+          <div class="about-section-content1">
+            <img src="./assets/images/about-avatar-group.png" alt="" />
+            <h4>Still have questions?</h4>
+            <p>
+              Can’t find the answer you’re looking for? <br />
+              Please chat to our friendly team.
+            </p>
+            <!-- HTML !-->
+            <button class="button-9" role="button">Get in touch</button>
+          </div>
+          <div class="about-section-content2">
+        
+  
+            <div
+              style="
+                visibility: hidden;
+                position: absolute;
+                width: 0px;
+                height: 0px;
+              "
+            >
+              <svg xmlns="http://www.w3.org/2000/svg">
+                <symbol viewBox="0 0 24 24" id="expand-more">
+                  <path d="M16.59 8.59L12 13.17 7.41 8.59 6 10l6 6 6-6z" />
+                  <path d="M0 0h24v24H0z" fill="none" />
+                </symbol>
+                <symbol viewBox="0 0 24 24" id="close">
+                  <path
+                    d="M19 6.41L17.59 5 12 10.59 6.41 5 5 6.41 10.59 12 5 17.59 6.41 19 12 13.41 17.59 19 19 17.59 13.41 12z"
+                  />
+                  <path d="M0 0h24v24H0z" fill="none" />
+                </symbol>
+              </svg>
+            </div>
+  
+            <details open>
+              <summary>
+                Do you assist students who have completed Germen language get placed ?
+                <svg
+                  class="control-icon control-icon-expand"
+                  width="24"
+                  height="24"
+                  role="presentation"
+                >
+                  <use
+                    xmlns:xlink="http://www.w3.org/1999/xlink"
+                    xlink:href="#expand-more"
+                  />
+                </svg>
+                <svg
+                  class="control-icon control-icon-close"
+                  width="24"
+                  height="24"
+                  role="presentation"
+                >
+                  <use
+                    xmlns:xlink="http://www.w3.org/1999/xlink"
+                    xlink:href="#close"
+                  />
+                </svg>
+              </summary>
+              <p>Yes, we have programs such as Dual Training Program, Nurses to Germany and Drivers to Germany Programs where 
+                we assist students who have passed the required level of German Language.</p>
+            </details>
+  
+            <details>
+              <summary>
+                Can I bring family to Germany?
+                <svg
+                  class="control-icon control-icon-expand"
+                  width="24"
+                  height="24"
+                  role="presentation"
+                >
+                  <use
+                    xmlns:xlink="http://www.w3.org/1999/xlink"
+                    xlink:href="#expand-more"
+                  />
+                </svg>
+                <svg
+                  class="control-icon control-icon-close"
+                  width="24"
+                  height="24"
+                  role="presentation"
+                >
+                  <use
+                    xmlns:xlink="http://www.w3.org/1999/xlink"
+                    xlink:href="#close"
+                  />
+                </svg>
+              </summary>
+              <p>Yes, you can bring family members. Specific requirements vary, so check with German authorities or the embassy 
+                for details.</p>
+            </details>
+  
+            <details>
+              <summary>
+                Can i work as a Nurse in Germany without language training ? 
+                <svg
+                  class="control-icon control-icon-expand"
+                  width="24"
+                  height="24"
+                  role="presentation"
+                >
+                  <use
+                    xmlns:xlink="http://www.w3.org/1999/xlink"
+                    xlink:href="#expand-more"
+                  />
+                </svg>
+                <svg
+                  class="control-icon control-icon-close"
+                  width="24"
+                  height="24"
+                  role="presentation"
+                >
+                  <use
+                    xmlns:xlink="http://www.w3.org/1999/xlink"
+                    xlink:href="#close"
+                  />
+                </svg>
+              </summary>
+              <p>No, language proficiency is crucial. It's highly recommended to undergo German language training, especially 
+                in professions like nursing.</p>
+            </details>
+            <details>
+              <summary>
+                How much does the whole program cost
+                <svg
+                  class="control-icon control-icon-expand"
+                  width="24"
+                  height="24"
+                  role="presentation"
+                >
+                  <use
+                    xmlns:xlink="http://www.w3.org/1999/xlink"
+                    xlink:href="#expand-more"
+                  />
+                </svg>
+                <svg
+                  class="control-icon control-icon-close"
+                  width="24"
+                  height="24"
+                  role="presentation"
+                >
+                  <use
+                    xmlns:xlink="http://www.w3.org/1999/xlink"
+                    xlink:href="#close"
+                  />
+                </svg>
+              </summary>
+              <p>Cost varies based on program, duration, and location. Research specific programs for accurate estimates. 
+                Scholarships and aid may be available.</p>
+            </details>
+            <details>
+              <summary>
+                Will you assist for stay in Germany?
+                <svg
+                  class="control-icon control-icon-expand"
+                  width="24"
+                  height="24"
+                  role="presentation"
+                >
+                  <use
+                    xmlns:xlink="http://www.w3.org/1999/xlink"
+                    xlink:href="#expand-more"
+                  />
+                </svg>
+                <svg
+                  class="control-icon control-icon-close"
+                  width="24"
+                  height="24"
+                  role="presentation"
+                >
+                  <use
+                    xmlns:xlink="http://www.w3.org/1999/xlink"
+                    xlink:href="#close"
+                  />
+                </svg>
+              </summary>
+              <p>Institutions and employers often offer support for accommodation, cultural orientation, and administrative 
+                processes. Check with them for details and consider local expat communities for additional resources.</p>
+            </details>
+          </div>
+        </div>
+      </div>  
+
+    <!-- end faq  -->
+
+</div>
+
+
+<?php include("header/footer.php") ?>
+
+<script>
+  fetch('./header.html')
+    .then(response => response.text())
+    .then(html => document.getElementById('header').innerHTML = html);
+
+  fetch('./footer.html')
+    .then(response => response.text())
+    .then(html => document.getElementById('footer').innerHTML = html);
+
+</script>
+
+  
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js"></script>
+
+<script src="https://code.jquery.com/jquery-3.6.4.min.js"></script>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js"></script>
+
+  <script>
+    $(document).ready(function() {
+      $('#heroCarousel').carousel({
+        interval: 3000
+      });
+    });
+  </script>
+
+<script>
+    $(document).ready(function() {
+      $('#imageCarousel').carousel({
+        interval: 3000
+      });
+    });
+  </script>
+
+<script>
+    const slider1 = document.querySelector(".slider1");
+    let card1Width = document.querySelector(".card1").offsetWidth;
+
+    // Clone the slider1 content for continuous scrolling
+    slider1.innerHTML += slider1.innerHTML;
+
+    function scrollslider1(direction) {
+        if (direction === 1) {
+            slider1.scrollBy({
+                left: card1Width + 20,
+                behavior: 'smooth'
+            });
+        } else if (direction === -1) {
+            slider1.scrollBy({
+                left: -card1Width - 20,
+                behavior: 'smooth'
+            });
+        } else {
+            slider1.scrollBy({
+                left: card1Width + 20,
+                behavior: 'smooth'
+            });
+        }
+
+        // Reset scroll position to the beginning when it reaches the cloned set
+        if (slider1.scrollLeft >= slider1.scrollWidth / 2) {
+            slider1.scrollTo({
+                left: 0,
+                behavior: 'auto'
+            });
+        }
+    }
+
+    // Automatic scroll every 3 seconds
+    setInterval(scrollslider1, 3000);
+</script>
+
+
+<!-- flipcard -->
+<script>
+    // script.js
+    document.getElementById('flip-btn-1').addEventListener('click', function() {
+  console.log("Button clicked!");  // Check if the button click is being captured
+  const card = document.getElementById('card-1-flip');
+  console.log(card);  // Check if the card element is being selected
+  card.classList.add("active");
+  });
+    document.getElementById('flip-btn-2').addEventListener('click', function() {
+  console.log("Button clicked!");  // Check if the button click is being captured
+  const card = document.getElementById('card-1-flip');
+  console.log(card);  // Check if the card element is being selected
+  card.classList.remove("active");
+  });
+
+    document.getElementById('flip-btn-3').addEventListener('click', function() {
+  console.log("Button clicked!");  // Check if the button click is being captured
+  const card = document.getElementById('card-2-flip');
+  console.log(card);  // Check if the card element is being selected
+  card.classList.add("active");
+  });
+    document.getElementById('flip-btn-4').addEventListener('click', function() {
+  console.log("Button clicked!");  // Check if the button click is being captured
+  const card = document.getElementById('card--flip');
+  console.log(card);  // Check if the card element is being selected
+  card.classList.remove("active");
+  });
+
+</script>
+
+
+</body>
+</html>
